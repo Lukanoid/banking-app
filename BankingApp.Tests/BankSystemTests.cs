f0f65e5e3ba4f0cd537b3f283e17892e8afb071e
Vasil Stamboliiski <vasil.stamboliiski1@gmail.com>
2026-05-15 12:58:10 +0300

    Added folder for test and setuped dependencies for future unit test writing

---
+++ BankingApp.Tests/BankSystemTests.cs
@@ -0,0 +1,20 @@
+using BankingApp.Core;
+using Xunit;
+
+namespace BankingApp.Tests
+{
+    public class BankSystemTests
+    {
+        [Fact]
+        public void CreateAccount_ShouldAddAccountToAccountList()
+        {
+            BankSystem bankSystem = new BankSystem();
+
+            BankAccount account = bankSystem.CreateAccount("John Doe");
+
+            Assert.Single(bankSystem.Accounts);
+            Assert.Equal("John Doe", account.OwnerName);
+            Assert.Contains(account, bankSystem.Accounts);
+        }
+    }
+}
