64cec7da4cac32b3d1231c54a5f522a3823efcf2
Vasil Stamboliiski <vasil.stamboliiski1@gmail.com>
2026-06-19 21:36:00 +0300

    Added test for Transaction constructor with valid data

---
+++ BankingApp.Tests/TransactionTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using BankingApp.Core;
+
+namespace BankingApp.Tests
+{
+    public class TransactionTests
+    {
+        [Fact]
+        public void Constructor_ShouldCreateTransaction_WhenDataIsValid()
+        {
+            Transaction transaction = new Transaction(TransactionType.Deposit, 100m);
+
+            Assert.Equal(TransactionType.Deposit, transaction.Type);
+            Assert.Equal(100m, transaction.Amount);
+            Assert.True(transaction.Date <= DateTime.Now);
+        }
+    }
+}
