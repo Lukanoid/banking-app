6f71e66ccdad9de4f2fc0372adc0c4cb44faa82d
Vasil Stamboliiski <vasil.stamboliiski1@gmail.com>
2026-06-26 14:14:51 +0300

    Added tests for Operation Result

---
+++ BankingApp.Tests/OperationResultTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+using BankingApp.Core;
+
+
+namespace BankingApp.Tests
+{
+    public class OperationResultTests
+    {
+        [Fact]
+        public void Constructor_ShouldCreateSuccessfulResult_WhenDataIsValid()
+        {
+            OperationResult result = new OperationResult(true, "Deposit successful.");
+
+            Assert.True(result.IsSuccess);
+            Assert.Equal("Deposit successful.", result.Message);
+        }
+
+        [Fact]
+        public void Constructor_ShouldCreateFailedResult_WhenDataIsValid()
+        {
+            OperationResult result = new OperationResult(false, "Amount must be greater than 0.");
+
+            Assert.False(result.IsSuccess);
+            Assert.Equal("Amount must be greater than 0.", result.Message);
+        }
+    }
+}
