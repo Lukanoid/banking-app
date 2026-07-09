0788e4a4763968cbd97fe6e0527705d0d35655f4
Vasil Stamboliiski <vasil.stamboliiski1@gmail.com>
2026-07-09 08:50:20 +0300

    Completed environment for api tests

---
+++ BankingApp.Api.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankingApp.Api.Persistence;
+using BankingApp.Core;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace BankingApp.Api.Tests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Testing");
+
+            builder.ConfigureServices(services =>
+            {
+                services.RemoveAll<BankSystem>();
+                services.RemoveAll<IBankStorage>();
+
+                services.AddSingleton<BankSystem>();
+                services.AddSingleton<IBankStorage, TestBankStorage>();
+            });
+        }
+    }
+}
---
+++ BankingApp.Api.Tests/TestBankStorage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankingApp.Api.Persistence;
+using BankingApp.Core;
+
+namespace BankingApp.Api.Tests
+{
+    public class TestBankStorage : IBankStorage
+    {
+        public List<BankAccount> SavedAccounts { get; set; } = new List<BankAccount>();
+
+        public List<BankAccount> LoadAccounts()
+        {
+            return new List<BankAccount>();
+        }
+
+        public void SaveAccounts(IReadOnlyList<BankAccount> accounts)
+        {
+            SavedAccounts = accounts.ToList();
+        }
+    }
+}
